3a30f33abbdbdc25a73734e404eeb2f7c0538c3f
elielbianchi <elielbianchi@hotmail.com>
2022-07-12 23:48:07 -0300

    feat: adicionado tema escuro e troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+import { AppThemeProvider } from "./contexts";
 import { AppRoutes } from "./routes/index";
-import { LightTheme } from "./themes";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 };
