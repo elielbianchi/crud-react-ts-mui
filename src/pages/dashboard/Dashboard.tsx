e60eba512f6dc6f255eb3ddc65432cfc1cbf0365
elielbianchi <elielbianchi@hotmail.com>
2022-07-17 15:49:08 -0300

    feat: criado layout base de pagina

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+import { LayoutBaseDePagina } from '../../layouts';
+
+export const Dashboard = () => {
+  return (
+    <LayoutBaseDePagina titulo="Página inicial">Testando</LayoutBaseDePagina>
+  );
+};
