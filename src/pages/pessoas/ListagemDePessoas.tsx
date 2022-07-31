c8b5a119faf6674a4f5808142313346f30eb4c72
elielbianchi <elielbianchi@hotmail.com>
2022-07-31 19:56:18 -0300

    feat: cria hook useDebounce e utiliza no input de busca

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -4,21 +4,25 @@ import { useSearchParams } from 'react-router-dom';
 import { PessoasService } from '../../services/api/pessoas/PessoasService';
 import { FerramentasDaListagem } from '../../components';
 import { LayoutBaseDePagina } from '../../layouts';
+import { useDebounce } from '../../hooks';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        alert(result.message);
-      } else {
-        console.log(result);
-      }
+    debounce(() => {
+      PessoasService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          console.log(result);
+        }
+      });
     });
   }, [busca]);
 
